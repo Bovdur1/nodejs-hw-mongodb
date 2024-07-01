2a10121b53f5a81f138bade0f912354a35fea2fb
Daria Tuz <daratuz41@gmail.com>
2024-07-01 16:51:47 +0300

    first commit

---
+++ src/db/models/contact.js
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Contact = model('contacts', contactSchema);
